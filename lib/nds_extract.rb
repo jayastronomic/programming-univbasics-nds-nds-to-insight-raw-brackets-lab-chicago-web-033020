bdd3be243ad9d98fec4095e3bf198d20aa5e4ee5
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-26 02:10:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,6 +2,19 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-
-
+  result = {}
+  director_index = 0
+  while director_index < nds.length do
+    director = nds[director_index][:name]
+    movie_index = 0
+    result[director] = 0
+    while movie_index < nds[director_index][:movies].length do
+      result[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    director_index += 1
+  end
+  result
 end
+
+
